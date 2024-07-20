bbd0a0291cd936fe68aa964c6075c91ea4c7e0c3
dsn777 <dsn300703@mail.ru>
2024-07-20 20:16:21 +0300

    test method was added and also commit command was tested

---
+++ src/main/java/com/example/Booking/Models/BookingRequest.java
@@ -0,0 +1,21 @@
+package com.example.Booking.Models;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingRequest {
+    private Date checkin;
+    private Date checkout;
+    private Integer adults;
+    private Integer children;
+
+    public boolean incomplete() {
+        return checkin == null || !(checkout != null & adults != null);
+    }
+}
